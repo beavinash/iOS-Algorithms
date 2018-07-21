082217f808d98cbe5fc8320f74fd97f986fde433
Avinash Reddy <beavinash15@gmail.com>
2018-07-21 15:10:04 -0700

    restart of the algorithms.

--- Numbers/RandomNumber.playground/Contents.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-
-func randomNumberGeneration(minimum: Int, maximum: Int) -> Int {
-    return Int(arc4random_uniform(UInt32(maximum - minimum + 1))) + minimum
-}
-
-randomNumberGeneration(minimum: 1, maximum: 5)
-randomNumberGeneration(minimum: 12, maximum: 12)
